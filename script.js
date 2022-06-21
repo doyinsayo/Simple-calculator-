81285885e6dda5b41b645668c557ddc2d5a5a7da
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-06-21 22:19:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// program for a simple calculator
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const num1 = parseFloat(prompt('Enter first number: '));
+const num2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = num1 + num2;
+}
+else if (operator == '-') {
+    result = num1 - num2;
+}
+else if (operator == '*') {
+    result = num1 * num2;
+}
+else {
+    result = num1 / num2;
+}
+
+// display the result
+console.log(`${num1} ${operator} ${num2} = ${result}`);
